022b818a2284d3f3af23a04c91e42205c01c68e8
Time-Coder <30686643+Time-Coder@users.noreply.github.com>
2026-04-11 18:28:02 +0800

    Delete PyUSD directory

--- PyUSD/Geom/__init__.py
+++
@@ -1,7 +0,0 @@
-from .xform import Xform
-from .sphere import Sphere
-
-__all__ = [
-    "Xform",
-    "Sphere"
-]
--- PyUSD/__init__.py
+++
@@ -1,7 +0,0 @@
-from .stage import Stage
-from .prim import Prim
-
-__all__ = [
-    "Stage",
-    "Prim"
-]
